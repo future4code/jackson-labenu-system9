b01682887037106a8febd5ae5f48831db852ef29
Alice <alicepsouzaba1998@gmail.com>
2020-11-07 23:36:15 -0300

    Initial settings

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export type MainType = {
+  name: string,
+  email: string,
+  birth_date: Date,
+  mission_id: number
+}
+
+export type MissionType = {
+  name: string,
+  start_date: Date,
+  end_date: Date,
+  module: number
+}
+
