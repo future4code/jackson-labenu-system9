407e2833f233af5581139d43d49a970d008e763b
Alice <alicepsouzaba1998@gmail.com>
2021-01-19 21:55:22 -0300

    UUID at hobby

--- src/controller/createHobby.ts
+++ src/controller/createHobby.ts
@@ -1,16 +1,15 @@
 import { Request, Response } from "express"
-import { insertHobby } from "../data/insertHobby"
+import { hobbyDatabase } from "../data/HobbyDatabase";
+import { idGenerator } from "../services/generateId";
 
 export const createHobby = async (
   req: Request, 
   res: Response
 ) => {
   try {
-    const { 
-      id, 
-      name 
-    } = req.body
-    await insertHobby(id, name)
+    const id = idGenerator.generate();
+    const { name } = req.body
+    await hobbyDatabase.createHobby(id, name)
     res.status(200).send("Novo hobby criado!")
   } catch (error) {
     res.status(400).send({ message: error.message })
